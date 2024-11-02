a1aceb6e2b34fdef108fd26a38e0a389175a3dd6
zhan0858 <zhan0858@algonquinlive.com>
2024-11-02 17:26:28 -0400

    import http and add http request from random-data-api

--- bin/exercise4.dart
+++ bin/exercise4.dart
@@ -1,5 +1,32 @@
-import 'package:exercise4/exercise4.dart' as exercise4;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${exercise4.calculate()}!');
+Future<void> fetchUsers() async {
+  final url = Uri.parse(
+      'https://random-data-api.com/api/v3/projects/265f8441-dbb3-419c-a375-9dc4e4cb1c1e?api_key=iAlJDBHolm8DzSlYeeaGoQ');
+  try {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodedJson = json.decode(response.body);
+
+      final List<Map<String, dynamic>> users =
+          List<Map<String, dynamic>>.from(decodedJson['data']);
+
+      for (var user in users) {
+        final uuid = user['uuid'];
+        final firstName = user['first_name'];
+        final lastName = user['last_name'];
+        print("$firstName $lastName's uuid: $uuid");
+      }
+    } else {
+      print('Failed to load users. Status Code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('An error occurred: $e');
+  }
+}
+
+void main() {
+  fetchUsers();
 }
